e38ead58b4e2f14a7a3af56432c9bf9c690a756e
Fathah KA <48355244+fathah@users.noreply.github.com>
2026-02-01 08:14:21 +0530

    ENV Names Update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,8 +9,6 @@ module.exports = {
         DATABASE_URL: process.env.DATABASE_URL,
         ZDRIVE_KEY: process.env.ZDRIVE_KEY,
         ZDRIVE_SECRET: process.env.ZDRIVE_SECRET,
-        NEXT_PUBLIC_ZAUTH_KEY: process.env.NEXT_PUBLIC_ZAUTH_KEY,
-        NEXT_PUBLIC_ZDRIVE_ROOT: process.env.NEXT_PUBLIC_ZDRIVE_ROOT,
         PORT: 3137
       },
     },
